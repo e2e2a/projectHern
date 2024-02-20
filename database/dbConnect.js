bfe828225d0a1e15e673c7983428e6832c68446c
e2e2a <emonawong22@gmail.com>
2024-02-20 08:18:01 +0800

    add

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -2,8 +2,8 @@ const { default: mongoose } = require("mongoose");
 
 const dbConnect = () => {
     try{
-        // const conn =  mongoose.connect(process.env.MONGODB_CONNECT_URI);
-        const conn = mongoose.connect('mongodb://127.0.0.1/hernan');
+        const conn =  mongoose.connect(process.env.MONGODB_CONNECT_URI);
+        // const conn = mongoose.connect('mongodb://127.0.0.1/hernan');
         console.log('database connected');
         return conn;
     } catch (error){
