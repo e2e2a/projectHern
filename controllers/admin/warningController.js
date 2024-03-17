dafb7eea24cf6d2ca3026780d139110d615bbc3d
e2e2a <emonawong22@gmail.com>
2024-03-17 10:49:19 +0800

    add

--- controllers/admin/warningController.js
+++ controllers/admin/warningController.js
@@ -47,17 +47,13 @@ const cronStart = async () => {
                             if (deceased.guardianEmail && deceased.guardianEmail.trim() !== '') {
                                 console.log('Guardian email:', deceased.guardianEmail, 'fiveYearsLater:',fiveYearsLater);
                                 const emailContent = `
-                                <div style="background-color: #f2f2f2; padding: 10px; width: 60%; text-align: justify;">
-                                <h2 style="color: #000;">Subject:  Gravesite Renewal</h2> <br/>
-                                <h3 style="color: #000;">Dear ${deceased.guardian},</h3>
-                                <br/>
-                                <p style="color: #000;">We hope this message finds you well. We wanted to remind you that your Yearly responsibility is currently overdue. We understand that life can get busy, so we're reaching out to gently encourage you to renew your pass at your earliest convenience.</p>
-                                <br/>
-                                <p style="color: #000;">By renewing your pass, you'll continue to have access to our services and be part of our community. If you have any questions or need assistance with the renewal process, please don't hesitate to contact us. We're here to help!</p>
-                                <br/>
-                                <p style="color: #000;">Thank you for your attention to this matter, and we appreciate your continued support.</p>
-                                <br/>
-                                <p style="color: #000;">Warm regards,<br/><a href="polanco-registrar.onrender.com">[POLANCO CIVIL REGISTRAR]</a></p>
+                                <div style="background-color: #f2f2f2; padding: 20px; border-radius: 10px; box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);">
+                                    <h2 style="color: #333;">Subject: Burial Exceed 5 Years (Due Date)</h2>
+                                    <h3 style="color: #333; margin-top: 20px;">Dear <span style="font-weight: normal;">${deceased.guardian},</span></h3>
+                                    <p style="color: #333; text-align: justify; margin-top: 20px;">We hope this message finds you well. We wanted to remind you that your yearly responsibility is currently overdue. We understand that life can get busy, so we're reaching out to gently encourage you to renew your pass at your earliest convenience.</p>
+                                    <p style="color: #333; text-align: justify; margin-top: 20px;">By renewing your pass, you'll continue to have access to our services and be part of our community. If you have any questions or need assistance with the renewal process, please don't hesitate to contact us. We're here to help!</p>
+                                    <p style="color: #333; text-align: justify; margin-top: 20px;">Thank you for your attention to this matter, and we appreciate your continued support.</p>
+                                    <p style="color: #333; text-align: justify; margin-top: 20px;">Warm regards,<br/><a href="http://polanco-registrar.onrender.com" style="color: #007bff; text-decoration: none;">[POLANCO CIVIL REGISTRAR]</a></p>
                                 </div>
                                 `;
                                 await sendEmail(
